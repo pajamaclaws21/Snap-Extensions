cabb1ff184c916807656b38f9bfcde7089daf167
PJ <95423707+pajamaclaws21@users.noreply.github.com>
2023-05-30 14:27:15 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+SnapExtensions.primitives.set(
+    'reload_test(x)',
+    function (x) {
+        return x;
+    }
+);
+
+SnapExtensions.primitives.set(
+    'working()',
+    function () {
+        return true;
+    }
+);
